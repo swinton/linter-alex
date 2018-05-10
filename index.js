53e676eac79850d679e0b7cc208a7735130488f3
Steve Winton <stevewinton@gmail.com>
2018-05-10 17:57:04 -0500

    Create an in-progress check run (WIP!)

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 module.exports = (robot) => {
-  robot.on('check_suite', context => {
-    // robot.log('Yay, check_suite was requested!')
-    context.log(context.payload)
+  robot.on('check_suite', async context => {
+    let {action} = context.payload
+    context.log(`action is "${action}".`)
+
+    // Create an in_progress check_run
+    const result = await context.github.checks.create({
+      owner: "swinton",
+      repo: "example",
+      name: "feedback",
+      head_branch: "helloworld",
+      head_sha: "2e3d00a6f14a667d50ad9ccd6f3dcfded52121e2",
+      status: "in_progress",
+      started_at: (new Date()).toISOString(),  // The time that the check run began in ISO 8601 format: `YYYY-MM-DDTHH:MM:SSZ`
+      completed_at: (new Date()).toISOString(),
+      conclusion: "status"
+    })
+
+    context.log(`result is ${result}.`)
   })
 }
