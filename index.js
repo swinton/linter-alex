119d5ea96c30da6e95120ef61781593bfcc2311f
Steve Winton <stevewinton@gmail.com>
2018-05-11 10:30:05 -0500

    Drop down to octokit.request to create in_progress check run

--- index.js
+++ index.js
@@ -3,19 +3,21 @@ module.exports = (robot) => {
     let {action} = context.payload
     context.log(`action is "${action}".`)
 
-    // Create an in_progress check_run
-    const result = await context.github.checks.create({
-      owner: "swinton",
-      repo: "example",
-      name: "feedback",
-      head_branch: "helloworld",
-      head_sha: "2e3d00a6f14a667d50ad9ccd6f3dcfded52121e2",
-      status: "in_progress",
-      started_at: (new Date()).toISOString(),  // The time that the check run began in ISO 8601 format: `YYYY-MM-DDTHH:MM:SSZ`
-      completed_at: (new Date()).toISOString(),
-      conclusion: "status"
+    const result = await context.github.request({
+      method: 'POST',
+      url: 'https://api.github.com/repos/swinton/example/check-runs',
+      name: 'feedback',
+      head_branch: 'helloworld',
+      head_sha: '2e3d00a6f14a667d50ad9ccd6f3dcfded52121e2',
+      details_url: 'https://example.com/#/checks/42',
+      external_id: '42',
+      status: 'in_progress',
+      started_at: (new Date()).toISOString(),
+      headers: {
+        accept: 'application/vnd.github.antiope-preview+json'
+      }
     })
 
-    context.log(`result is ${result}.`)
+    context.log('result is %j.', result)
   })
 }
