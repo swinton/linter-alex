ade561777877253a4e68ecc1cc0a810d93e8eb78
Steve Winton <stevewinton@gmail.com>
2018-05-11 16:27:23 -0500

    Provide (static) analysis

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ module.exports = (robot) => {
       context.log('creating in_progress check run...')
 
         let url = `https://api.github.com/repos/${owner}/${repo}/check-runs`
-        const result = await context.github.request(Object.assign(headers, {
+        let result = await context.github.request(Object.assign(headers, {
           method: 'POST',
           url: url,
           name: 'feedback',
@@ -27,7 +27,10 @@ module.exports = (robot) => {
           started_at: (new Date()).toISOString(),
         }))
 
+        const {data: {id: check_run_id, url: check_run_url}} = result
         context.log('result is %j.', result)
+        context.log(`check_run_id is ${check_run_id}.`)
+        context.log(`check_run_url is ${check_run_url}.`)
 
         // TODO
         // Process all .md files in this repo
@@ -37,8 +40,34 @@ module.exports = (robot) => {
         const {data: {content: encoded}} = await context.github.repos.getContent(path)
         const decoded = Buffer.from(encoded, 'base64').toString()
         const analysis = alex.markdown(decoded).messages
-
         context.log('analysis is %j.', analysis)
+
+        // Provide feedback
+        // https://developer.github.com/v3/checks/runs/#update-a-check-run
+        // PATCH /repos/:owner/:repo/check-runs/:check_run_id
+        result = await context.github.request(Object.assign(headers, {
+          method: 'PATCH',
+          url: check_run_url,
+          head_branch: branch,
+          head_sha: sha,
+          status: 'completed',
+          conclusion: 'neutral',
+          completed_at: (new Date()).toISOString(),
+          output: {
+            title: 'analysis',
+            summary: '[Alex](http://alexjs.com/) found 1 issue',
+            annotations: [{
+              filename: 'HELLO_ALEX.md',  // The name of the file to add an annotation to.
+              blob_href: 'https://github.com/swinton/example/blob/c017292677f172a6a283d8a81810421d2209d822/HELLO_ALEX.md', // The file's full blob URL.
+              start_line: 3, // The start line of the annotation.
+              end_line: 3, // The end line of the annotation.
+              warning_level: 'notice', // The warning level of the annotation. Can be one of notice, warning, or failure.
+              message: "`his` may be insensitive, use `their`, `theirs`, `them` instead", // A short description of the feedback for these lines of code. The maximum size is 64 KB.
+              title: "her-him" // The title that represents the annotation. The maximum size is 255 characters.
+            }]
+          }
+        }))
+
     }
   })
 }
