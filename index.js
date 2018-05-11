4777fb95a86f9deff6120ee39c5358c159f265e0
Steve Winton <stevewinton@gmail.com>
2018-05-11 15:59:17 -0500

    Perform analysis of HELLO_ALEX.md

--- index.js
+++ index.js
@@ -1,23 +1,44 @@
+const alex = require('alex')
+const mediaType = 'application/vnd.github.antiope-preview+json'
+const headers = {headers: {accept: mediaType}}
+
 module.exports = (robot) => {
   robot.on('check_suite', async context => {
-    let {action} = context.payload
+    const {action, check_suite} = context.payload
+    const {owner, repo} = context.repo()
+    const {head_branch: branch, head_sha: sha} = check_suite
+
     context.log(`action is "${action}".`)
+    context.log(`repo is "${owner}/${repo}".`)
+    context.log(`branch is "${branch}".`)
+    context.log(`sha is "${sha}".`)
 
-    const result = await context.github.request({
-      method: 'POST',
-      url: 'https://api.github.com/repos/swinton/example/check-runs',
-      name: 'feedback',
-      head_branch: 'helloworld',
-      head_sha: '2e3d00a6f14a667d50ad9ccd6f3dcfded52121e2',
-      details_url: 'https://example.com/#/checks/42',
-      external_id: '42',
-      status: 'in_progress',
-      started_at: (new Date()).toISOString(),
-      headers: {
-        accept: 'application/vnd.github.antiope-preview+json'
-      }
-    })
+    if (action === 'requested') {
+      context.log('creating in_progress check run...')
 
-    context.log('result is %j.', result)
+        let url = `https://api.github.com/repos/${owner}/${repo}/check-runs`
+        const result = await context.github.request(Object.assign(headers, {
+          method: 'POST',
+          url: url,
+          name: 'feedback',
+          head_branch: branch,
+          head_sha: sha,
+          status: 'in_progress',
+          started_at: (new Date()).toISOString(),
+        }))
+
+        context.log('result is %j.', result)
+
+        // TODO
+        // Process all .md files in this repo
+
+        // Process HELLO_ALEX.md
+        const path = context.repo({path: 'HELLO_ALEX.md', ref: branch})
+        const {data: {content: encoded}} = await context.github.repos.getContent(path)
+        const decoded = Buffer.from(encoded, 'base64').toString()
+        const analysis = alex.markdown(decoded).messages
+
+        context.log('analysis is %j.', analysis)
+    }
   })
 }
