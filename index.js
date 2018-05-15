ac5d0f61bec53a9efadb5c7f4acf69d1d3505b18
Steve Winton <stevewinton@gmail.com>
2018-05-15 11:30:50 -0500

    Analyze entire tree on check_suite.requested

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const analyzeTree = require('./lib/analysis')
 const mediaType = 'application/vnd.github.antiope-preview+json'
 const headers = {headers: {accept: mediaType}}
 
 module.exports = (robot) => {
   robot.on('check_suite', async context => {
+    const analyzeTree = require('./lib/analysis')(context)
     const {action, check_suite} = context.payload
     const {owner, repo} = context.repo()
     const {head_branch: branch, head_sha: sha} = check_suite
@@ -33,8 +33,9 @@ module.exports = (robot) => {
         context.log(`check_run_url is ${check_run_url}.`)
 
         // Process all .md files in this repo
-        const annotations = await analyzeTree(context.github, owner, repo, sha)
-        console.log('annotations are %j', annotations)
+        const annotations = (await analyzeTree(owner, repo, sha))
+          .filter(annotation => annotation.length > 0)
+        context.log('annotations are %j', annotations)
 
         // Provide feedback
         // https://developer.github.com/v3/checks/runs/#update-a-check-run
