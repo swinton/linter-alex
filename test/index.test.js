4ea106fa1990e213c33a43dd9293ecf3a59e56bb
Steve Winton <stevewinton@gmail.com>
2018-07-18 18:35:11 -0500

    Expand assertions when no annotations found

--- test/index.test.js
+++ test/index.test.js
@@ -31,15 +31,13 @@ describe('index', () => {
 
     // Initialize app with robot instance
     app(robot)
-
+  
     // Mock out the GitHub API
     github = {}
     github.request = jest
       .fn()
-      .mockResolvedValueOnce({data: {id: 42, url: "https://api.github.com/repos/foo/bar/check-runs/42"}})
-      .mockResolvedValueOnce({data: {id: 42, url: "https://api.github.com/repos/foo/bar/check-runs/42"}})
-
-    analyzeTree.mockResolvedValueOnce([])
+      .mockResolvedValueOnce({data: {id: 42, url: "https://api.github.com/repos/wintron/example/check-runs/42"}})
+      .mockResolvedValueOnce({data: {id: 42, url: "https://api.github.com/repos/wintron/example/check-runs/42"}})
 
     // Pass mocked out GitHub API into out robot instance
     robot.auth = () => Promise.resolve(github)
@@ -49,8 +47,36 @@ describe('index', () => {
     global.Date = RealDate
   })
   
-  it('works', async () => {
+  it('finds no annotations', async () => {
+    analyzeTree.mockResolvedValueOnce([])
+
     await robot.receive(event)
+
+    expect(github.request).toHaveBeenCalledTimes(2)
+    expect(github.request).toHaveBeenNthCalledWith(1, {
+      headers: {
+        'accept': 'application/vnd.github.antiope-preview+json'
+      },
+      method: 'POST',
+      url: 'https://api.github.com/repos/wintron/example/check-runs',
+      name: 'feedback',
+      head_branch: 'example',
+      head_sha: '9875bf915c118e6369a610770288cf7f0a415124',
+      status: 'in_progress',
+      started_at: '2018-01-01T00:00:00.000Z'
+    })
+    expect(github.request).toHaveBeenNthCalledWith(2, {
+      headers: {
+        'accept': 'application/vnd.github.antiope-preview+json'
+      },
+      method: 'PATCH',
+      url: 'https://api.github.com/repos/wintron/example/check-runs/42',
+      head_branch: 'example',
+      head_sha: '9875bf915c118e6369a610770288cf7f0a415124',
+      status: 'completed',
+      conclusion: 'success',
+      completed_at: '2018-01-01T00:00:00.000Z'
+    })
 
     expect(analyzeTree).toHaveBeenCalledTimes(1)
     expect(analyzeTree.mock.calls[0][0]).toBeDefined()
